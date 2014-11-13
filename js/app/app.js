e160b1e8cf2e73a7f3547d6c61e7da0992cdd859
fishermanswharff <fishermanswharff@github.com>
2014-11-13 16:09:57 -0500

    auto pushed jw-red updates

--- js/app/app.js
+++ js/app/app.js
@@ -1,7 +1,6 @@
 function trace(){ for(var i = 0, count = arguments.length; i < count; i++){console.log(arguments[i]);}};
 
 var Router = Backbone.Router.extend({
-
   routes: {
     '':'home',
     'projects':'projects',
