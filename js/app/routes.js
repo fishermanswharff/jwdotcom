3a4bfb5b903129f984fed21094fc4e2db08438f5
fishermanswharff <fishermanswharff@github.com>
2014-11-13 16:51:30 -0500

    auto pushed jw-red updates

--- js/app/routes.js
+++ js/app/routes.js
@@ -12,20 +12,23 @@ var Router = Backbone.Router.extend({
   },
 
   projects: function(){
-
+    trace('hello world');
   },
 
   resume: function(){
     var template = Handlebars.compile($("#resumeTemplate").html());
-    
+    $(".view").html(template({
+      data: data;
+    }));
   }
 
 });
-
+var data;
 var router = new Router();
 Backbone.history.start();
 $(document).ready(function(){
   $.getJSON('data/resume.json', function(json, textStatus) {
-      trace(json, textStatus);
+    data = json;  
+    trace(json, textStatus);
   });
 });
