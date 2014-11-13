efc9426fc10dc203cf1f160553be0000c7a99077
fishermanswharff <fishermanswharff@github.com>
2014-11-13 17:59:40 -0500

    auto pushed jw-red updates

--- js/app/app.js
+++ js/app/app.js
@@ -1,8 +1,8 @@
 function trace(){ for(var i = 0, count = arguments.length; i < count; i++){console.log(arguments[i]);}};
-var data;
-$(document).ready(function(){
-  $.getJSON('data/resume.json', function(json, textStatus) {
+var data = $.getJSON('data/resume.json', function(json, textStatus) {
     data = json;
     trace(data);
   });
+$(document).ready(function(){
+  
 });
