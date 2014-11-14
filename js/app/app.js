729506dfddbc4cb8fce2e6101964b8f8e5590a65
fishermanswharff <fishermanswharff@github.com>
2014-11-14 16:23:52 -0500

    auto pushed jw-red updates

--- js/app/app.js
+++ js/app/app.js
@@ -9,6 +9,6 @@ App.activateNavigation = function(e){
 $(document).ready(function(){
   $("nav#main-nav").on("click","a#menu-button",function(event){
     App.activateNavigation(event);
-    trace($(this));
+    $(this).toggleClass("active");
   });
 });
