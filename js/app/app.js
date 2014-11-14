62fc0393c094d69f5df02c0e9427c466b3b593ff
fishermanswharff <fishermanswharff@github.com>
2014-11-14 00:00:50 -0500

    auto pushed jw-red updates

--- js/app/app.js
+++ js/app/app.js
@@ -1,1 +1,13 @@
 function trace(){ for(var i = 0, count = arguments.length; i < count; i++){console.log(arguments[i]);}};
+
+var App = App || {};
+
+App.activateNavigation = function(e){
+  trace("clicking menu icon");
+};
+
+$(document).ready(function(){
+  $("a#menu-icon").on("click",function(event){
+    App.activateNavigation(event);
+  });
+});
