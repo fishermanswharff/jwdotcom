a95b9f4dd91f708130fd8effa5119ecfc1da247c
fishermanswharff <fishermanswharff@github.com>
2014-11-15 15:27:04 -0500

    auto pushed jw-red updates

--- js/app/app.js
+++ js/app/app.js
@@ -2,7 +2,7 @@ function trace(){ for(var i = 0, count = arguments.length; i < count; i++){conso
 
 var App = {
   windowSize: '',
-  windowWidth = 0;
+  windowWidth: 0;
   actualSize: 0,
   firstRun: false,
   isIE: false
