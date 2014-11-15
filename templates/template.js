ed786b271dc53be3476925b9a3e97875b7d0f068
fishermanswharff <fishermanswharff@github.com>
2014-11-15 12:46:41 -0500

    auto pushed jw-red updates

--- templates/template.js
+++ templates/template.js
@@ -28,10 +28,17 @@ templates['home'] = template({"1":function(depth0,helpers,partials,data) {
   return buffer + "      </ul>\n    </div>\n  </div>\n</div>";
 },"useData":true});
 templates['projects'] = template({"1":function(depth0,helpers,partials,data) {
-  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-  return "    <div class=\"project\">\n      <h1>"
+  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, buffer = "    <div class=\"project\">\n      <h1>"
     + escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper)))
-    + "</h1>\n    </div>\n";
+    + "</h1>\n      <ul>\n";
+  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.tech : depth0), {"name":"each","hash":{},"fn":this.program(2, data),"inverse":this.noop,"data":data});
+  if (stack1 != null) { buffer += stack1; }
+  return buffer + "      </ul>\n    </div>\n";
+},"2":function(depth0,helpers,partials,data) {
+  var lambda=this.lambda, escapeExpression=this.escapeExpression;
+  return "        <li>"
+    + escapeExpression(lambda(depth0, depth0))
+    + "</li>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var stack1, buffer = "<section class=\"projects\">\n";
   stack1 = helpers.each.call(depth0, ((stack1 = (depth0 != null ? depth0.data : depth0)) != null ? stack1.projects : stack1), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
