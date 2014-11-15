86b7a7d9dd83464ccf262097ed67c37011ba2db2
fishermanswharff <fishermanswharff@github.com>
2014-11-15 13:50:06 -0500

    auto pushed jw-red updates

--- templates/template.js
+++ templates/template.js
@@ -52,10 +52,10 @@ templates['projects'] = template({"1":function(depth0,helpers,partials,data) {
     + escapeExpression(lambda(depth0, depth0))
     + "</li>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var stack1, buffer = "<section class=\"projects\">\n";
+  var stack1, buffer = "<div class=\"projects\">\n";
   stack1 = helpers.each.call(depth0, ((stack1 = (depth0 != null ? depth0.data : depth0)) != null ? stack1.projects : stack1), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
   if (stack1 != null) { buffer += stack1; }
-  return buffer + "</section>\n";
+  return buffer + "</div>\n";
 },"useData":true});
 templates['resume'] = template({"1":function(depth0,helpers,partials,data) {
   var helper, lambda=this.lambda, escapeExpression=this.escapeExpression, functionType="function", helperMissing=helpers.helperMissing;
